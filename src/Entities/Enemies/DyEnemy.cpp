feb24ab7278f3d2352468f5133f2354d73ca31ef
TruffleTheProto <angel.perez109@upr.edu>
2026-03-13 23:34:46 -0400

    DyEnemy should change sprites but I am not sure.

--- src/Entities/Enemies/DyEnemy.cpp
+++ src/Entities/Enemies/DyEnemy.cpp
@@ -3,8 +3,8 @@
 void DyEnemy::draw() {
     if (HitBox::drawHitbox) this->hitBox.draw();
     DrawTexturePro(ImageManager::SpriteSheet, this->texture, 
-                Rectangle{2, 128, 13, 14}, 
-                Vector2{15, 15}, this->aimAngle, WHITE);
+        Rectangle{this->position.first + 15, this->position.second + 15, 30, 30}, 
+        Vector2{15, 15}, this->aimAngle, WHITE);
 }
 
 void DyEnemy::update(std::pair<float, float> pos, HitBox target) {
